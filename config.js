c0595815a023d84e0f5eacbb35050983712d9eda
filipuswork <filipus351@guru.sma.belajar.id>
2026-09-21 03:04:25 +0000

    config: pasang URL Apps Script asli

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 /* URL Google Apps Script Web App (WAJIB DIISI)
    Cara mendapat: buat Apps Script → Deploy → Web App → salin URL.
    Contoh: 'https://script.google.com/macros/s/AKfycbx5m2-kFHh-zKDU3ert9mEaOTb2tqVZ_ASiwZPYfT-8TSvovDrU2E5MnJ7cHLccQ58sAQ/exec' */
-window.SERVER_URL = 'https://script.google.com/macros/s/AKfyc.../exec';
+window.SERVER_URL = 'https://script.google.com/macros/s/AKfycbx5m2-kFHh-zKDU3ert9mEaOTb2tqVZ_ASiwZPYfT-8TSvovDrU2E5MnJ7cHLccQ58sAQ/exec';
 
 /* Durasi ujian dalam menit */
 window.DURASI_MENIT = 60;
